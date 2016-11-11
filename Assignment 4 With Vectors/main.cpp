466f14e2223c456b21b639e1a87898fd8c558644
Axel Garcia <axelgar1123@gmail.com>
2016-11-11 15:50:13 -0800

    Initial Commit

---
+++ Assignment 4 With Vectors/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Assignment 4 With Vectors
+//
+//  Created by Axel Garcia on 11/11/16.
+//  Copyright © 2016 Axel Garcia. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
